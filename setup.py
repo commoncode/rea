8b4812310363549ab5439ce37f69ad00aeed38eb
Daryl Antony <daryl@commoncode.com.au>
2012-11-14 23:07:06 +1100

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup( name='rea',
+    version = '0.0.1',
+    description = 'Resources, Events & Agents marketplace models',
+    author = 'Daryl Antony',
+    author_email = 'daryl@commoncode.com.au',
+    url = 'https://github.com/commoncode/rea',
+    keywords = ['django',],
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+        'Django>=1.4',
+        'django-polymorphic',
+    ]
+)
