9f02b739b63e7b8b2e6fab4d3fcec5055e33aec0
Adam Hoddinott <adam@messymind.net>
2013-11-13 15:19:00 +1100

    Rename checkout contract to salesorder

--- rea/market/patterns/salesorder/models.py
+++ rea/market/patterns/salesorder/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from rea.models.contracts import Contract
 from django_xworkflows import models as xwf_models
-from rea.market.patterns.checkout.workflows import CheckoutWorkflow
+from rea.market.patterns.salesorder.workflows import SalesOrderWorkflow
 
 
-class CheckoutContract(Contract):
+class SalesOrderContract(Contract):
     """
     A product subscription contract.
 
     Contains a list of clauses that must be satisfied for
     state continuation.
     """
-    state = xwf_models.StateField(CheckoutWorkflow)
+    state = xwf_models.StateField(SalesOrderWorkflow)
 
     customer = models.ForeignKey('market.Customer')
     enterprise = models.ForeignKey('market.Enterprise')
--- rea/market/patterns/salesorder/workflows.py
+++ rea/market/patterns/salesorder/workflows.py
@@ -2,15 +2,15 @@ from django_xworkflows import models as xwf_models
 from django.utils.translation import ugettext_lazy as _
 
 
-class CheckoutWorkflow(xwf_models.Workflow):
+class SalesOrderWorkflow(xwf_models.Workflow):
     """
-    States of a checkout based transaction
+    States of a sales order based transaction
     """
     states = (
         # Our initial state
         ('init', _(u"Initial State")),
         # New commitment to pay within 30 days
-        ('commitment', _(u"Committed to pay for checkout")),
+        ('commitment', _(u"Committed to pay for sales orders")),
         # The event of exchanging resources
         ('event', _(u"Payment recieved")),
     )
