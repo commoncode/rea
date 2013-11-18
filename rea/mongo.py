6ca2e737443fe22f87b6aeac63c0dd452b65f3c0
Adam Hoddinott <adam@messymind.net>
2013-11-18 12:05:29 +1100

    Send mongo update to any related models

--- rea/mongo.py
+++ rea/mongo.py
@@ -3,6 +3,7 @@ import pymongo
 import importlib
 
 from django.conf import settings
+from django.db.models.fields.related import ForeignRelatedObjectsDescriptor
 from denormalize.backend.mongodb import MongoBackend
 from denormalize.models import DocumentCollection
 
@@ -14,6 +15,14 @@ REA_MONGO_DB_NAME = getattr(settings,
     "REA_MONGO_DB_NAME", "rea_denormalized")
 REA_MONGO_CONNECTION_URI = getattr(settings,
     "REA_MONGO_URI", "mongodb://localhost")
+
+
+def import_from_string(string):
+    mods = string.split('.')
+    class_str = mods[-1]
+    path_str = '.'.join(mods[:-1])
+    module = importlib.import_module(path_str)
+    return getattr(module, class_str)
 
 
 class REAMongoBackend(MongoBackend):
@@ -34,34 +43,36 @@ class REAMongoBackend(MongoBackend):
 
     def deleted(self, collection, doc_id):
         mongoID = self.get_mongo_id(collection, doc_id)
+        current_collection = collection
         while True:
             has_parent = False
-            table_name = collection.name
-            if collection.parent_collection:
+            table_name = current_collection.name
+            if current_collection.parent_collection:
                 table_name = self.get_parent_table_name(
-                    collection, table_name
+                    current_collection, table_name
                 )
                 has_parent = True
 
-            logging.debug('deleted: %s %s', collection.name, doc_id)
-            col = getattr(self.db, collection.name)
+            logging.debug('deleted: %s %s', current_collection.name, doc_id)
+            col = getattr(self.db, current_collection.name)
             col.remove({'_id': mongoID})
 
             if not has_parent:
                 break
-            collection = collection.parent_collection
+            current_collection = current_collection.parent_collection
 
     def added(self, collection, doc_id, doc):
         # We can share the same ID across
         mongoID = self.get_mongo_id(collection, doc_id)
         is_parent = True
         parent_table_name = collection.name
+        current_collection = collection
         while True:
             has_parent = False
-            table_name = collection.name
-            if collection.parent_collection:
+            table_name = current_collection.name
+            if current_collection.parent_collection:
                 table_name = self.get_parent_table_name(
-                    collection, table_name
+                    current_collection, table_name
                 )
                 has_parent = True
 
@@ -75,22 +86,23 @@ class REAMongoBackend(MongoBackend):
 
             if not has_parent:
                 break
-            collection = collection.parent_collection
+            current_collection = current_collection.parent_collection
             is_parent = False
 
     def changed(self, collection, doc_id, doc):
         mongoID = self.get_mongo_id(collection, doc_id)
+        current_collection = collection
 
         while True:
             has_parent = False
-            table_name = collection.name
-            if collection.parent_collection:
+            table_name = current_collection.name
+            if current_collection.parent_collection:
                 table_name = self.get_parent_table_name(
-                    collection, table_name
+                    current_collection, table_name
                 )
                 has_parent = True
 
-            logging.debug('changed: %s %s', collection.name, doc_id)
+            logging.debug('changed: %s %s', current_collection.name, doc_id)
             col = getattr(self.db, table_name)
             # We are not allowed to update _id
             if '_id' in doc:
@@ -102,7 +114,54 @@ class REAMongoBackend(MongoBackend):
 
             if not has_parent:
                 break
-            collection = collection.parent_collection
+            current_collection = current_collection.parent_collection
+
+        serializer_class = collection.serializer_class
+        model = serializer_class.Meta.model
+        model_instance = model.objects.get(id=doc_id)
+
+        # Get a list of reverse foreign keys our model could have
+        if isinstance(serializer_class, str):
+            serializer_class = import_from_string(serializer_class)
+        reversed_f_keys = [
+            attr for attr in model.__dict__.values()
+            if isinstance(attr, ForeignRelatedObjectsDescriptor)
+        ]
+
+        # TODO: Why is customer being saved twice?
+        for f_key in reversed_f_keys:
+            # Get the reverse lookup model manager
+            reverse_lookup = getattr(
+                model_instance, f_key.related.get_accessor_name())
+
+            # Check to see if the model is part of the mongo collection
+            # saving process
+            _collection = [
+                x for x in self.collections.values()
+                if x.model == reverse_lookup.model
+            ]
+
+            if len(_collection):
+                col = _collection[0]
+
+                while True:
+                    if col is None:
+                        break
+
+                    document_ids = reverse_lookup.values_list('id', flat=True)
+                    for d_id in document_ids:
+                        self._call_changed(col, d_id)
+
+                    if col.parent_collection:
+                        _sub_collection = [
+                            x for x in self.collections.values()
+                            if x.model == col.parent_collection.model
+                        ]
+                        if len(_sub_collection):
+                            col = _sub_collection[0]
+                            continue
+                    col = None
+
 
     def get_doc(self, collection, doc_id):
         mongoID = self.get_mongo_id(collection, doc_id)
@@ -150,12 +209,7 @@ class DRFDocumentCollection(DocumentCollection):
         Use Django Rest Framework to serialize our object
         """
         if isinstance(self.serializer_class, str):
-            # If a string, import and get the class
-            mods = self.serializer_class.split('.')
-            class_str = mods[-1]
-            path_str = '.'.join(mods[:-1])
-            module = importlib.import_module(path_str)
-            self.serializer_class = getattr(module, class_str)
+            self.serializer_class = import_from_string(self.serializer_class)
 
         data = self.serializer_class(obj).data
         logger.debug('\033[94m%s:\033[0m %s' % (model._meta.db_table, data))
