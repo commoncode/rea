c4fc5358b57a7765a1040a2b7c4f3c349ab537b8
Chris Morgan <me@chrismorgan.info>
2014-04-01 14:33:32 +1100

    Update for cqrs refactoring.

--- rea/models/agents.py
+++ rea/models/agents.py
@@ -1,6 +1,6 @@
 from entropy.base import SlugMixin, TitleMixin
 
-from cqrs.mongo import CQRSPolymorphicModel
+from cqrs.models import CQRSPolymorphicModel
 
 
 class Agent(CQRSPolymorphicModel, TitleMixin):
